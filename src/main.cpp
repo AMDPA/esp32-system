0639fe3735605b652a90d86739a3ca1745555e31
Luiz Felipe <fc671219@gmail.com>
2021-07-25 10:42:58 -0300

    Reimportaçao de classes da main

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,14 @@
 #include <Arduino.h>
 
 //#include "classes/Chuva.h"
-#include "classes/Cjmcu.h"
-#include "classes/Hidrogenio.h"
-#include "classes/Luminosidade.h"
-#include "classes/TempUmidAr.h"
-#include "classes/UmidSolo.h"
+#include "classes/modules/CO2/Cjmcu.h"
+#include "classes/modules/Hidrogenio/Hidrogenio.h"
+#include "classes/modules/Luminosidade/Luminosidade.h"
+#include "classes/modules/TempUmidAr/TempUmidAr.h"
+#include "classes/modules/UmidSolo/UmidSolo.h"
 
-#include "classes/LeitorCartao.h"
-#include "classes/Json.h"
+#include "classes/modules/LeitorCartao/LeitorCartao.h"
+#include "classes/system/Json.h"
 //#include "classes/Ble.h"
 
 Cjmcu cjmcu;
@@ -23,8 +23,8 @@ unsigned long millisLeitura = millis();
 
 void setup(){
     Serial.begin(115200);
-    
-    leitorCartao.initSD();
+
+    //leitorCartao.initSD();
 
     cjmcu.init();
     hidrogenio.init();
@@ -32,10 +32,10 @@ void setup(){
     tempUmidAr.init();
     umidSolo.init();
 
-    if(!leitorCartao.fileExists("/medicoes4.csv")){
+    /*if(!leitorCartao.fileExists("/medicoes4.csv")){
         leitorCartao.createFile("/medicoes4.csv");
         leitorCartao.writeFile("/medicoes4.csv", "cjmcu_co2, cjmcu_etvoc, hidrogenio_ppm, luminosidade_percent, temUmidAr_humidity, tempUmidAr_temperature, tempUmidAr_heatIndex, umidSolo_percent\n");
-    }
+    }*/
 }
 
 void loop(){
@@ -51,7 +51,7 @@ void loop(){
 
         msg += String(cjmcu.getEco2()) + ", ";
         msg += String(cjmcu.getEtvoc()) + ", ";
-        
+
         msg += String(hidrogenio.getPpm()) + ", ";
 
         msg += String(luminosidade.getPercent()) + ", ";
@@ -61,7 +61,7 @@ void loop(){
         msg += String(tempUmidAr.getHeatIndex()) + ", ";
 
         msg += String(umidSolo.getValue()) + "\n";
-        
+
         //leitorCartao.writeFile("/medicoes4.csv", msg);
         Serial.println(msg);
         millisLeitura = millis();
