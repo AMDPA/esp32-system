cb48c5a36f363a39a3a39bdcef42dcac37829d27
Luiz Felipe <fc671219@gmail.com>
2021-10-12 16:23:05 -0300

    Adicionado server, fix json formato.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <Arduino.h>
+#include <HTTPClient.h>
 
 #include "classes/modules/Chuva/Chuva.h"
 #include "classes/modules/CO2/Cjmcu.h"
@@ -20,7 +21,6 @@ TempUmidAr _tempUmidAr;
 UmidSolo _umidSolo;
 Chuva _chuva;
 
-//Ble _ble;
 Hora _hora;
 JsonESP32 _json;
 ServerESP32 _server;
@@ -30,30 +30,39 @@ void setup(){
     Serial.begin(115200);
 
     _energia.reestartSystemTokePin();
-    if(_energia.wakeup_reason == ESP_SLEEP_WAKEUP_TOUCHPAD){
-      _server.config();
-      _server.init();
-      while(true);
+    _server.config();
+
+    if(_energia.wakeup_reason == ESP_SLEEP_WAKEUP_TOUCHPAD || _server.user.length() == 0 || _server.id_esta == -1){
+        _server.init(true);
+        while(true);
+    }
+
+    Serial.println("SERVER MODE: " + String(_server.mode));
+    if(_server.mode){
+        _server.init();
+        _hora.updateHoraRede();
     }
 
     _leitorCartao.initSD();
-    _cjmcu.init();
+    /*_cjmcu.init();
     _hidrogenio.init();
     _luminosidade.init();
     _tempUmidAr.init();
     _umidSolo.init();
+    _chuva.init();*/
 }
 
 void loop(){
-    Serial.print("-");
-   
-    Serial.println("LOOP");
+Serial.println(_hora.getDataFull());
+Serial.println(_hora.getUnixTimeStamp());
     _server.finish();
-    _cjmcu.update();
+
+    /*_cjmcu.update();
     _hidrogenio.update();
     _luminosidade.update();
     _tempUmidAr.update();
     _umidSolo.update();
+    _chuva.update();*/
 
     if(!_leitorCartao.fileExists("/" + _hora.getData() + ".json")){
         _leitorCartao.createFile("/" + _hora.getData() + ".json");
@@ -61,20 +70,20 @@ void loop(){
 
     DynamicJsonDocument doc1(24576);
 
-    JsonObject doc_0 = doc1.createNestedObject(String(_hora.getUnixTimeStamp()));
-    doc_0["hora"] = _hora.getDataFull();
-
-    JsonObject doc_0_cjmcu = doc_0.createNestedObject("cjmcu");
-    doc_0_cjmcu["eco2"] = _cjmcu.getEco2();
-    doc_0_cjmcu["etvoc"] = _cjmcu.getEtvoc();
-
-    doc_0["hidrogenio"] = _hidrogenio.getPpm();
-    doc_0["luminosidade"] = _luminosidade.getPercent();
-
-    JsonObject doc_0_tempUmidAr = doc_0.createNestedObject("tempUmidAr");
-    doc_0_tempUmidAr["umidade"] = _tempUmidAr.getHumidity();
-    doc_0_tempUmidAr["temperatura"] = _tempUmidAr.getTemperature();
-    doc_0_tempUmidAr["heatIndex"] = _tempUmidAr.getHeatIndex();
+    JsonObject ssss = doc1.createNestedObject(String(_hora.getUnixTimeStamp()));
+    ssss["hora"] = _hora.getUnixTimeStamp();
+    ssss["dht11_temperatura"] = _tempUmidAr.getTemperature();
+    ssss["dht11_umidade"] = _tempUmidAr.getHumidity();
+    ssss["dht11_indiceCalor"] = _tempUmidAr.getHeatIndex();
+    ssss["cjmcu101_luminosidade"] = _luminosidade.getPercent();
+    ssss["soilmoisture_umidade"] = _umidSolo.getValue();
+    ssss["ccs811_co2"] = _cjmcu.getEco2();
+    ssss["ccs811_etvoc"] = _cjmcu.getEtvoc();
+    ssss["mq8_hidrogenio"] = _hidrogenio.getPpm();
+    ssss["mhrd_chuva"] = _chuva.getStatus();
+    ssss["a18b20_temperatura"] = 0.0;
+    ssss["ph4502c_ph"] = 0.0;
+    ssss["mhz19b_co2"] = 0.0;
 
     String msg = _leitorCartao.readFile("/" + _hora.getData() + ".json");
     String msg2;
@@ -94,16 +103,28 @@ void loop(){
     }
 
     _leitorCartao.writeFile("/" + _hora.getData() + ".json", msg2, true);
-    Serial.println(msg2);
 
-    if(_server.m.equals("REMOTO")){
-        String _msg = msg2;
-        serializeJson(doc1, msg);
+    if(_server.mode){
+        String _msg;
+        serializeJson(doc1, _msg);
+        _server.init();
 
-        //Quando tiver um servidor, imprementar o envio de dados aqui. Submeter a var _msg;
+        HTTPClient http;
+
+        http.begin("https://api-amacpa.herokuapp.com/");
+
+        http.addHeader("Content-Type", "application/json");
+        http.addHeader("User", _server.user);
+        http.addHeader("Estacao", String(_server.id_esta));
+
+        int code = http.POST(_msg);
+
+        Serial.println("CODE: " + code);
+        http.end();
     }
 
-    _energia.setDeepSleep(15 * 60);
+    //_energia.setDeepSleep(15 * 60);
+    delay(5000);
 
 }
 
